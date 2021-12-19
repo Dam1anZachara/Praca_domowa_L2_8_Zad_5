ea36aeddee1fd30395aa52bf91463a21b5a4e762
Damian Z <damian9203@gmail.com>
2021-12-19 16:55:49 +0100

    Add project files.

---
+++ L2_8_Zad_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L2_8_Zad_5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program dla liczb od 1 do 20 wyświetli na ekranie ich trzecią potęgę.");
+
+            int counter = 1;
+
+            while (counter <= 20)
+            {
+                double numberAfterPow = Math.Pow(counter, 3);
+                Console.WriteLine($"Trzeca potęga liczby {counter} to: {numberAfterPow}");
+                counter++;
+            }
+        }
+    }
+}
